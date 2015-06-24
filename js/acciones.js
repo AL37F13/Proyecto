07e82e3e03c6ed2ec1159d0a55b1aac6cbd14309
AL37F13 <lucero.rc.16@gmail.com>
2015-06-24 11:43:50 -0500

    uno
    aplicacion de proyecto

---
+++ js/acciones.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+$(document).ready(function(e) {
+//document.addEventListener("deviceready",function(){
+	$('#btnmenu').on('tap',function(){
+	navigator.vibrate(1000);
+});//tap a btnvibrar
+$('#btnuno').on('tap',function(){
+	navigator.vibrate(1000);
+});//tap a btnvibrar
+$('#btndos').on('tap',function(){
+	navigator.vibrate(1000);
+});//tap a btnvibrar
+$('#btntres').on('tap',function(){
+	navigator.vibrate(1000);
+});//tap a btnvibrar
+$('#btncuatro').on('tap',function(){
+	navigator.vibrate(1000);
+});//tap a btnvibrar
+$('#btncinco').on('tap',function(){
+	navigator.vibrate(1000);
+});//tap a btnvibrar
+//}); 
+});
+
