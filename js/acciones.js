eadf7b2b2be7a130d08df0dc400630654d23f5aa
AL37F13 <lucero.rc.16@gmail.com>
2015-06-25 10:03:55 -0500

    siete
    cambio en las imagenes

--- js/acciones.js
+++ js/acciones.js
@@ -3,22 +3,22 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	$('#btnmenu').on('tap',function(){
 	navigator.vibrate(1000);
-});//tap a btnvibrar
+});
 $('#btnuno').on('tap',function(){
 	navigator.vibrate(1000);
-});//tap a btnvibrar
+});
 $('#btndos').on('tap',function(){
 	navigator.vibrate(1000);
-});//tap a btnvibrar
+});
 $('#btntres').on('tap',function(){
 	navigator.vibrate(1000);
-});//tap a btnvibrar
+});
 $('#btncuatro').on('tap',function(){
 	navigator.vibrate(1000);
-});//tap a btnvibrar
+});
 $('#btncinco').on('tap',function(){
 	navigator.vibrate(1000);
-});//tap a btnvibrar
+});
 }); 
 });
 
